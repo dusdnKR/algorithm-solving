d9484bc5d0f00fba8d7bdb90ace19c6944bdb272
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-08-18 01:23:45 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 1
+    a, b = 1, 1
+    
+    if n < 2:
+        return 1
+    
+    for i in range(n-1):
+        answer = a + b
+        a, b = b, answer
+        
+    answer %= 1234567
+    
+    return answer
