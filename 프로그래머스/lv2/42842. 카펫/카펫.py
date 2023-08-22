f2eea13d8b6190cfc31667adb835f448c06cd7e1
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-08-22 23:44:44 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,7 +1,5 @@
 def solution(brown, yellow):
     answer = []
-    
-    x, y = 0, 0
     total = brown + yellow
     
     for y in range(1, total+1):
@@ -10,6 +8,5 @@ def solution(brown, yellow):
             if 2*x + 2*y - 4 == brown and (x-2) * (y-2) == yellow:
                 answer = [x, y]
                 break
-        else: continue
     
     return answer
