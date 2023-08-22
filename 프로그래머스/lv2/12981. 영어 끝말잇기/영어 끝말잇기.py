cdf18b7fabcba0abbdb7bb07e90cad48a4d1a45f
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-08-23 00:13:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,27 @@
+def solution(n, words):
+    answer = [0, 0]
+    log = []
+    
+    for i, word in enumerate(words):
+        if i == 0:
+            log.append(word)
+            continue
+        else:
+            if words[i-1][-1] != word[0]:
+                answer = [(i+1)%n, (i+1)/n]
+                if answer[0] == 0:
+                    answer[0] = n
+                if answer[1] % 1 != 0:
+                    answer[1] = int(answer[1])+1
+                break
+            if word in log:
+                answer = [(i+1)%n, (i+1)/n]
+                if answer[0] == 0:
+                    answer[0] = n
+                if answer[1] % 1 != 0:
+                    print(answer[1])
+                    answer[1] = int(answer[1])+1
+                break
+            log.append(word)
+    
+    return answer
