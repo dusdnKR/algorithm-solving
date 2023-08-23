83399a66a94800fa94da583315157314b7a2ff42
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-08-23 23:05:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4962.34 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,18 @@
+def solution(elements):
+    answer = 0
+    numOfElements = len(elements)
+    sumOfElements = 0
+    sumList = []
+    
+    elements += elements
+    
+    
+    
+    for n in range(1, numOfElements+1):
+        for m in range(numOfElements):
+            sumOfElements = sum(elements[m:m+n])
+            sumList.append(sumOfElements)
+    
+    answer = len(set(sumList))
+    
+    return answer
