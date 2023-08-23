c9f928cef88f3818033c144aa03559add5231ca6
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-08-24 00:39:38 +0900

    [level 2] Title: [1차] 캐시, Time: 0.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -8,21 +8,19 @@ def solution(cacheSize, cities):
     if cacheSize == 0: return len(cities) * 5
             
     for city in cities:
-        
         if len(cache) < cacheSize:
             if city not in cache:
                 answer += 5
             else:
                 answer += 1
                 cache.remove(city)
-            cache.append(city)
         else:
             if city in cache:
                 answer += 1
                 cache.remove(city)
             else:
                 answer += 5
-                if cache: del cache[0]
-            cache.append(city)
+                del cache[0]
+        cache.append(city)
     
     return answer
