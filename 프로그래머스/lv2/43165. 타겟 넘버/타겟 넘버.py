72b7c93de2e10671e5144697b46661880ed170de
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-08-30 22:44:18 +0900

    [level 2] Title: 타겟 넘버, Time: 13.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,22 @@
+def solution(numbers, target):
+    answer = 0
+    
+    binary = 2 ** (len(numbers))
+    binary_list = bin(binary)
+    binary_list = list(binary_list[3:])
+    binary_list[0] = '0'
+    
+    while "0" in binary_list:
+        total = 0
+        for i, num in enumerate(numbers):
+            if binary_list[i] == '0':
+                total += num
+            else:
+                total -= num
+        if total == target:
+            answer += 1
+        binary += 1
+        binary_list = bin(binary)
+        binary_list = list(binary_list[3:])
+    
+    return answer
