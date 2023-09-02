dba1ad37aa9552146cea835e0cd26db591a80e4b
dusdnKR <84877632+dusdnKR@users.noreply.github.com>
2023-09-02 17:28:30 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,15 +1,8 @@
-def convert_num(n):
-    if n == 1: return 1
-    elif n == 2: return 2
-    else: return 4
-
 def solution(n):
-    answer = []
-    while n >= 3:
-        answer.append(convert_num(n%3))
+    answer = ''
+    while n > 0:
+        if n % 3 == 1: answer += '1'
+        elif n % 3 == 2: answer += '2'
+        else: answer +='4'
         n = (n-1) // 3
-    if n > 0: answer.append(convert_num(n))
-
-    answer = ''.join(map(str, answer[::-1]))
-    
-    return answer
+    return answer[::-1]
