a31637ca9919bcaf51dea8be06201dda5a745573
Yeonwoo Kim <84877632+dusdnKR@users.noreply.github.com>
2024-01-24 21:02:15 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,14 +1,2 @@
 def solution(a, b):
-    
-    answer = 0
-    
-    if a < b:
-        for i in range(a, b+1):
-            answer += i
-    elif a > b:
-        for i in range(b, a+1):
-            answer += i
-    else:
-        answer = a
-        
-    return answer
+    return sum(range(min(a, b), max(a, b)+1))
